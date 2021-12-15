4c57101c8a8c08c754b228037db36b4de402ac2d
xbxcw <hanyuncheng@outlook.com>
2021-12-15 13:01:17 +0800

    添加github hosts

---
+++ hosts.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from time import sleep
+URL = r'http://tool.chinaz.com/dns/?type=1&host=github.com&ip='
+HOSTS = r"C:\Windows\System32\drivers\etc\hosts"
+chrome_opts = webdriver.ChromeOptions()
+chrome_opts.add_argument("--headless")
+
+browser = webdriver.Chrome(chrome_options=chrome_opts)
+browser.get(URL)
+while True:
+    try:
+        info = browser.find_element_by_xpath('/html/body/div[2]/div[2]/ul/li[11]/div[2]/p').text.split(' ', 1)
+        if info:
+            break
+    except:
+        print('---')
+    sleep(1)
+    
+browser.quit()
+
+data = info[0]+' '+'github.com # hyc'
+
+with open(HOSTS,'r',encoding='utf8') as f:
+    lines = f.readlines()
+
+
+with open(HOSTS,'w',encoding='utf8')as f_w:
+    for line in lines:
+        if 'github.com # hyc' in line:
+            continue
+        f_w.write(line)
+    f_w.write(data)
+
